c5d27937f21dca71e99666316e7def6b8adb2759
fordream <35677218+OctoberGOD@users.noreply.github.com>
2020-08-20 09:07:40 +0800

    Create 左右遍历.java

---
+++ 32.最长有效括号/左右遍历.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int left=0,right=0,maxLength=0;
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i)=='(')    left++;
+            else    right++;
+            if(left==right) {
+                maxLength=Math.max(maxLength,right*2);
+            }else if(left<right){
+                left=0;
+                right=0;
+            }
+        }
+
+        left=0;
+        right=0;
+        for(int i=s.length()-1;i>=0;i--) {
+            if(s.charAt(i)=='(')    left++;
+            else    right++;
+            if(left==right) {
+                maxLength=Math.max(maxLength,right*2);
+            }else if(left>right){
+                left=0;
+                right=0;
+            }
+        }
+        return maxLength;
+    }
+}
